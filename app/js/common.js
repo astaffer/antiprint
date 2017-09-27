77b29dd9b7537826bd7f09738a65cce6636b408d
Astaffer <astaffer@gmail.com>
2017-09-27 23:59:47 +0300

    add mmenu

--- app/js/common.js
+++ app/js/common.js
@@ -27,4 +27,24 @@ $(function() {
       directionNav: false,
       move: 2
   });
+   var $menu = $("#menu").mmenu();
+    var $icon = $("#my-icon");
+    var API = $menu.data( "mmenu" );
+
+    $icon.on( "click", function() {
+       API.open();
+    });
+
+    API.bind( "open:finish", function() {
+       setTimeout(function() {
+          $icon.addClass( "is-active" );
+       }, 100);
+    });
+    API.bind( "close:finish", function() {
+       setTimeout(function() {
+          $icon.removeClass( "is-active" );
+       }, 100);
+    });
+
+    $('.mm-title').text("Меню");
 });
--- gulpfile.js
+++ gulpfile.js
@@ -29,6 +29,7 @@ gulp.task('js', ['common-js'], function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
 		'app/libs/flexslider/jquery.flexslider-min.js',
+		'app/libs/jquery.mmenu/jquery.mmenu.js',
 		'app/js/common.min.js', // Всегда в конце
 		])
 	.pipe(concat('scripts.min.js'))
